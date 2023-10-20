286f75295dfc80f3e0d028b34024b24cef52ef83
Safeer Alam <safeeralam997@gmail.com>
2023-10-20 03:25:19 -0700

    netlify build attemp 2 , removed warnings!

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function About(props) {
-  // const [myStyle,setMyStyle] = useState({
-  //     color:'black',
-  //     backgroundColor:'white'
-  // })
-
   let myStyle = {
     color: props.mode === "dark" ? "white" : "black",
     backgroundColor: props.mode === "dark" ? "#2e5c85" : "white",
